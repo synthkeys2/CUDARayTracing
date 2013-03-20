f70d86770d077952c1b5d5f89553cc111ec427ec
Phil Stehle <philstehle@gmail.com>
2013-03-20 03:17:44 -0400

    Begin shape implementation

--- source/Shape.cpp
+++ source/Shape.cpp
@@ -1,1 +1,13 @@
 #include "Shape.h"
+
+
+Sphere::Sphere(Point center, float radius)
+	:	mCenter(center), mRadius(radius)
+{
+
+}
+
+bool Sphere::Intersect(Ray* ray, HitInfo* hitInfo)
+{
+
+}
--- source/Shape.h
+++ source/Shape.h
@@ -11,10 +11,21 @@ class Shape
 {
 public:
 	Shape();
-	~Shape();
+	virtual ~Shape();
 
 	virtual bool Intersect(Ray* ray, HitInfo* hitInfo) = 0;
 
+};
+
+class Sphere : public Shape
+{
+public:
+	Sphere(Point center, float radius);
+	~Sphere();
+
+	virtual bool Intersect(Ray* ray, HitInfo* hitInfo);
+
 private:
-	Point mLocation;
+	Point mCenter;
+	float mRadius;
 };
